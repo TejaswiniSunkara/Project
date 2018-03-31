53b19500397dba83a40307fc51fd44239eebc1e3
TejaswiniSunkara <37922528+TejaswiniSunkara@users.noreply.github.com>
2018-03-31 15:21:41 +0530

    Update Even.py

--- Even.py
+++ Even.py
@@ -1,5 +1,5 @@
 count = int(("how many even numbers you want to print: "))
-print(count+" even numbers are: ")
+print(count+" Even numbers are: ")
 for counter in range(0, count*2):
 	if(counter%2==0):
 		print(counter)
